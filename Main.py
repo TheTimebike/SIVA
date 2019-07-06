10d4902cb0f110c17912d09f7c005e1052afa7e8
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-07-06 19:02:41 +0100

    table updates

--- Main.py
+++ Main.py
@@ -132,6 +132,7 @@ def Main(packaged_data):
                 details = "Crucible, " + mode_data["displayProperties"]["name"]
                 picture = "crucible"
 
+        print(picture)
         RPC.update(
             state=state_conversion_table.get(state, state), 
             details=details_conversion_table.get(details, details),

2dce7a663d3a6ef1a3016645693965b1eb535fec
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-07-09 09:09:51 +0100

    Update Main.py

--- modules/Main.py
+++ modules/Main.py
@@ -44,23 +44,21 @@ class Requests:
         self.headers = {"X-API-Key": self.api_token}
 
     def get(self, request):
-        _data = _requests.get(urllib.parse.quote(request, safe=':/?&=,.'), headers=self.headers)
-        self._requestData = _data.json()
+        self._requestData = _requests.get(urllib.parse.quote(request, safe=':/?&=,.'), headers=self.headers).json()
         if self._requestData.get("ErrorCode", False) == 2101:
-            self.interface.error("1")
-            return
+            return self.interface.error("1")
         return self._requestData
 
 def get_last_played_id(membershipType, membershipID, requests):
-    url = CHARACTER_LOOKUP.format(membershipType, membershipID)
-    character_data = requests.get(url)
+    character_data = requests.get(CHARACTER_LOOKUP.format(membershipType, membershipID))
     epoch_character_table = {}
     for key, attr in character_data["Response"]["characters"]["data"].items():
         epoch_character_table[convert_datestring_to_epoch(attr["dateLastPlayed"])] = key
 
-    for epoch, id in epoch_character_table.items():
-        if epoch == max(epoch_character_table.keys()):
-            return id
+    [return id for epoch, id in epoch_character_table.items() if epoch == max(epoch_character_table.keys())]
+    #for epoch, id in epoch_character_table.items():
+    #    if epoch == max(epoch_character_table.keys()):
+    #        return id
 
 def convert_datestring_to_epoch(datestring):
     unix_epoch = datetime.datetime(1970, 1, 1)
@@ -74,8 +72,7 @@ class Main:
         self.directory = directory
 
     def start_siva(self, packaged_data, interface):
-        client_id = '596381603522150421'
-        RPC = Presence(client_id)
+        RPC = Presence('596381603522150421')
         RPC.connect()
 
         configurator = Config(self.directory)
@@ -90,8 +87,7 @@ class Main:
         user_membership_type = platform_enum_conversion_table[config["platform"]]
         user_membership_data = requests.get(MEMBERSHIP_ID_LOOKUP.format(user_membership_type, config["username"]))["Response"]
         if len(user_membership_data) == 0:
-            interface.error("2")
-            return
+            return interface.error("2")
 
         user_membership_id = user_membership_data[0]["membershipId"]
 
@@ -106,13 +102,7 @@ class Main:
                 state_conversion_table = configurator.get_conversion_table("state")
                 details_conversion_table = configurator.get_conversion_table("details")
 
-                activity_data = requests.get(
-                    ACTIVITY_LOOKUP.format(
-                        user_membership_type, 
-                        user_membership_id, 
-                        last_played_character
-                        )
-                    )
+                activity_data = requests.get(ACTIVITY_LOOKUP.format(user_membership_type, user_membership_id, last_played_character))
                 activity_hash = activity_data["Response"]["activities"]["data"]["currentActivityHash"]
                 activity_data_decoded = decoder.decode_hash(activity_hash, "DestinyActivityDefinition", "en")
 
@@ -125,14 +115,12 @@ class Main:
                 picture, timer = "in_orbit", time.time()
 
                 if mode_data != None:
-
                     details = mode_data["displayProperties"]["name"]
                     state = activity_data_decoded["displayProperties"].get("name", "In Orbit")
                     picture = activity_data_decoded["displayProperties"]["name"].lower().replace(" ", "_")
                     timer = convert_datestring_to_epoch(activity_data["Response"]["activities"]["data"]["dateActivityStarted"])
                     
-                    remove_list = [",", "(", ")", ":"]
-                    for char in remove_list:
+                    for char in [",", "(", ")", ":"]:
                         picture = picture.replace(char, "")
 
                     if activity_data_decoded["isPvP"]:

1341d4d24ad6a8d76ffff8c0c7355c7ed794cf14
TheTimebike <38054649+TheTimebike@users.noreply.github.com>
2019-07-09 09:12:33 +0100

    Update update.py

--- modules/update.py
+++ modules/update.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 
 def update(interface):
     data = get(interface.data["version_url"])
-    with open("./siva_files/siva_update.zip", "wb") as out:
+    with open("./{0}/siva_update.zip".format(interface.data["directory_name"]), "wb") as out:
         out.write(data.content)
     zip = zipfile.ZipFile("./siva_files/siva_update.zip", "r")
     zip.extractall("./")
